5e733447b032bcd6a21c14f3dc45d4a789a7ac5a
Alialsayad <20508000a@gmail.com>
2023-04-08 09:02:55 -0700

    a

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,43 +1,44 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <ctype.h>
 #include <string.h>
+#include <ctype.h>
+#include "main.h"
+
 /**
- * main - prints the num of args positive numbers
- * @argv: int
- * @argc: argument vector
- *
- * Return: 0
+ * main - Program that takes in all integer arguments and returns the sum
+ * @argc: Number of command line arguments
+ * @argv: Array name
+ * Return: 1 if a non-integer is among the passed in arguments, 0 otherwise
  */
+
 int main(int argc, char *argv[])
 {
-	int a;
-	unsigned int s, num = 0;
-	char *e;
+	int i, j, length, sum;
+	char *ptr;
 
-	if (argc > 1)
-	{
-	for (a = 1; a < argc; a++)
-	{
-		e = argv{a};
-
-		for (s = 0; s < strlen(e); s++)
-		{
-			if (e[s] < 48 || e[s] > 57)
-			{
-				printf("Error\n");
-				return (1);
-			}
-		}
-		num += atoi(e);
-		e++;
-	}
-	printf("%d\n", num)
-	}
+	if (argc < 2)
+		printf("0\n");
 	else
 	{
-	printf("0\n");
+		sum = 0;
+		for (i = 1; i < argc; i++)
+		{
+			ptr = argv[i];
+			length = strlen(ptr);
+
+			for (j = 0; j < length; j++)
+			{
+				if (isdigit(*(ptr + j)) == 0)
+				{
+					printf("Error\n");
+					return (1);
+				}
+			}
+
+			sum += atoi(argv[i]);
+		}
+
+	printf("%d\n", sum);
 	}
 	return (0);
 }
-
