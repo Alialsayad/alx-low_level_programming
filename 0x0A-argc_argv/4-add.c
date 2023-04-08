00de0482a0ff37a2e8d43518032a744fd088e138
Alialsayad <20508000a@gmail.com>
2023-04-07 18:23:44 -0700

    a

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - adds positive numbers
+ * @argv: int
+ * @argc: argument count
+ *
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int a;
+	unsigned int s, num = 0;
+	char *e;
+
+	if (argc > 1)
+	{
+	for (a = 1; a < argc; a++)
+	{
+		e = argv{a};
+
+		for (s = 0; s < strlen(e); s++)
+		{
+			if (e[s] < 48 || e[s] > 57)
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		num += atoi(e);
+		e++;
+	}
+	printf("%d\n", num)
+	}
+	else
+	{
+	printf("0\n");
+	}
+	return (0);
+}
+
