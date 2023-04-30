69a6067c3b1df40b4a78d64986ed15ad42c54ac4
Alialsayad <20508000a@gmail.com>
2023-04-30 16:40:12 -0700

    a

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -10,12 +10,12 @@
 
 void free_listint(listint_t *head)
 {
-	listint_t *al;
+	listint_t *node;
 
 	while (head)
 	{
-		al = head;
+		node = head;
 		head = head->next;
-		free(all);
+		free(node);
 	}
 }
