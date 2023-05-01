f5cd412a27a93d089c1723516192cc6ff71c35cc
Alialsayad <20508000a@gmail.com>
2023-05-01 14:46:55 -0700

    a

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -1,5 +1,8 @@
-#include "lists.h"
 #include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
 /**
  * add_nodeint_ind -adds anode to the bottom
  * @head: pointer to first node
