1563071acee4983735a550bbef8f6e807f1c67b4
Alialsayad <20508000a@gmail.com>
2023-05-01 15:26:25 -0700

    a

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -18,7 +18,6 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 	if (!head || !new_node)
 		return (NULL);
 
-	new_node->next = NULL;
 	new_node->n = n;
 	if (!*head)
 		*head = new_node;
@@ -28,6 +27,7 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 		while (node->next)
 			node = node->next;
 		node->next = new_node;
+		new_node->next = NULL;
 	}
 	return (new_node);
 }
